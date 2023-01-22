cb5c43517a8de590cf90ea2aa9db57e0c490bea8
rahuldurgapal <rahuldurgapal191@gmail.com>
2023-01-22 21:07:16 +0530

    update

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const http = require('http');
-const data=require('./data');
-http.createServer((req,res)=>{
+// const http = require('http');
+// const data=require('./data');
+// http.createServer((req,res)=>{
 
-    res.writeHead(200,{'Content-Type':'application\json'});
-    res.write(JSON.stringify(data));
-    res.end();
-}).listen(4500);
+//     res.writeHead(200,{'Content-Type':'application\json'});
+//     res.write(JSON.stringify(data));
+//     res.end();
+// }).listen(4500);
+
+
+const exp = require('express');
+const app = exp();
+
+app.get('',(req,res)=>{
+    res.send("Hello this is home page");
+});
+app.get('/about',(req,res)=>{
+    res.send("This is About Page");
+});
+
+app.get('/ContactUs',(req,res)=>{
+    res.send("This is Contact us Page");
+});
+
+app.listen(5000);
